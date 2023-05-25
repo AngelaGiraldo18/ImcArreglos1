f592037253ec298d410465ce4e15d4e163af1819
AngelaGiraldo18 <at593987@gmail.com>
2023-05-25 15:36:19 -0600

    first commit

---
+++ src/imcArreglos/PrincipalArreglos.java
@@ -0,0 +1,10 @@
+package imcArreglos;
+
+public class PrincipalArreglos {
+
+	public static void main(String[] args) {
+         ProcesosArreglos misProce = new ProcesosArreglos();
+         misProce.iniciar();
+	}
+
+}
---
+++ src/imcArreglos/ProcesosArreglos.java
@@ -0,0 +1,71 @@
+package imcArreglos;
+
+import javax.swing.JOptionPane;
+
+public class ProcesosArreglos {
+
+	public void iniciar() {
+		
+		menu();
+	}
+	
+	public void menu() {
+		 int opcion = 0;
+	        do {
+	            opcion = mostrarMenu();
+	            switch (opcion) {
+	                case 1:
+	                    calcularIMC();
+	                    break;
+	                case 2:
+	                    JOptionPane.showMessageDialog(null, "Saliendo del programa...");
+	                    break;
+	                default:
+	                    JOptionPane.showMessageDialog(null, "Opción inválida. Inténtalo nuevamente.");
+	                    break;
+	            }
+	        } while (opcion != 2);
+	}
+
+    public int mostrarMenu() {
+        String opcionStr = JOptionPane.showInputDialog(
+                "Calculadora de Índice de Masa Corporal (IMC)\n"
+                        + "1. Calcular IMC\n"
+                        + "2. Salir\n"
+                        + "INGRESE NUMERO");
+        return Integer.parseInt(opcionStr);
+    }
+
+    public static void calcularIMC() {
+        String nombre = JOptionPane.showInputDialog("Ingrese su nombre:");
+        double peso = Double.parseDouble(JOptionPane.showInputDialog("Ingrese su peso:"));
+        double altura = Double.parseDouble(JOptionPane.showInputDialog("Ingrese su talla:"));
+
+        double imc = peso / (altura * altura);
+        String mensaje = "Nombre: " + nombre + "\n"
+                + "Peso: " + peso + " kg\n"
+                + "Altura: " + altura + " m\n"
+                + "IMC: " + imc + "\n\n";
+
+        if (imc <= 18) {
+            mensaje += "Clasificación: Sufre de anorexia";
+        } else if (imc >= 18 && imc < 20) {
+            mensaje += "Clasificación: Sufre de delgadez";
+        } else if (imc >= 20 && imc < 27) {
+            mensaje += "Clasificación: Tiene peso normal";
+        } else if (imc >= 27 && imc < 30) {
+            mensaje += "Clasificación: Sufre de obesidad (grado 1)";
+        } else if (imc >= 30 && imc < 35) {
+            mensaje += "Clasificación: Sufre de obesidad (grado 2)";
+        } else if (imc >= 35 && imc < 40) {
+            mensaje += "Clasificación: Sufre de obesidad (grado 3)";
+        } else {
+            mensaje += "Clasificación: Sufre de obesidad morbida";
+        }
+
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
+		
+}
+
+
